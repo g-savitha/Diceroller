bb5c410f37a7f95acfbdbc334f500a40b4907cc3
savitha <g-savitha@github.com>
2021-09-30 16:52:07 +0530

    Updated the app with images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,8 +3,14 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,12 +22,31 @@ class MainActivity : AppCompatActivity() {
             val toast = Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
         }
     }
+    /**
+     * Roll the dice and update the screen with the result.
+     */
 
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView : TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        // Find the ImageView in the layout
+        val diceImage : ImageView = findViewById(R.id.imageView)
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when(diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+        // Update the content description for screen readers
+        diceImage.contentDescription = diceRoll.toString()
+
+
     }
 
     class Dice(private val numSides: Int){
